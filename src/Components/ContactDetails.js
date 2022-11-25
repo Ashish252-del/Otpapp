4fb441f91b05f815ad79aee9b5273616a469ae31
Ashish Patel <ashishpatel3946@gmail.com>
2022-11-25 18:59:01 +0530

    otp frontend

---
+++ src/Components/ContactDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ContactDetails = () => {
+  return (
+    <div>
+      <div className="card text-dark bg-info mb-3">
+        <div className="card-body">
+          <h5 className="card-title">Ashish</h5>
+          <p className="card-text">Address: Gorakhpur</p>
+          <h5 className="card-header">Contact Number :8869937308</h5>
+        </div>
+      </div>
+      <div className="card text-dark bg-info mb-3">
+        <div className="card-body">
+          <h5 className="card-title">Abhay</h5>
+          <p className="card-text">Address:Barabanki</p>
+          <h5 className="card-header">Contact Number :98568855</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactDetails;
